69cd8b0a34603b2893447bd40d75989163eff629
Josh <josharms00@gmail.com>
2022-04-30 20:45:51 -0400

    Added more tests for the matrix class.

--- tests/test.cpp
+++ tests/test.cpp
@@ -31,3 +31,189 @@ TEST_CASE("Matrix Inverse", "[matrix 4x4]")
 		}
 	}
 }
+
+TEST_CASE("Matrix Transpose", "[matrix 4x4]")
+{
+	mathz::Mat4 orig(
+		1.f, 3.f, 3.f, 3.f,
+		2.f, 3.f, 1.f, 3.f,
+		2.f, 4.f, 1.f, 3.f,
+		2.f, 5.f, 1.f, 3.f
+	);
+
+	mathz::Mat4 expected_transpose(
+		1.f, 2.f, 2.f, 2.f,
+		3.f, 3.f, 4.f, 5.f,
+		3.f, 1.f, 1.f, 1.f,
+		3.f, 3.f, 3.f, 3.f
+	);
+
+	mathz::Mat4 result = orig.transpose();
+
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		for (unsigned int j = 0; j < 4; ++j)
+		{
+			REQUIRE(result(i, j) == Approx(expected_transpose(i, j)).margin(0.0001).epsilon(1e-10));
+		}
+	}
+}
+
+TEST_CASE("Matrix Addition", "[matrix 4x4]")
+{
+	mathz::Mat4 a(
+		1.f, 3.f, 3.f, 3.f,
+		2.f, 3.f, 1.f, 3.f,
+		2.f, 4.f, 1.f, 3.f,
+		2.f, 5.f, 1.f, 3.f
+	);
+
+	mathz::Mat4 b(
+		1.2f, 2.2f, 2.3f, 22.4f,
+		3.2f, 3.2f, 4.4f, 5.34f,
+		3.49f, 1.1f, 1.4f, 12.1f,
+		77.f, 3.2f, 3.4f, 3.32f
+	);
+
+	mathz::Mat4 expected_addition(
+		2.2f, 5.2f, 5.3f, 25.4f,
+		5.2f, 6.2f, 5.4f, 8.34f,
+		5.49f, 5.1f, 2.4f, 15.1f,
+		79.f, 8.2f, 4.4f, 6.32f
+	);
+
+	mathz::Mat4 result = a + b;
+
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		for (unsigned int j = 0; j < 4; ++j)
+		{
+			REQUIRE(result(i, j) == Approx(expected_addition(i, j)).margin(0.0001).epsilon(1e-10));
+		}
+	}
+}
+
+TEST_CASE("Matrix Subtraction", "[matrix 4x4]")
+{
+	mathz::Mat4 a(
+		1.f, 3.f, 3.f, 3.f,
+		2.f, 3.f, 1.f, 3.f,
+		2.f, 4.f, 1.f, 3.f,
+		2.f, 5.f, 1.f, 3.f
+	);
+
+	mathz::Mat4 b(
+		1.2f, 2.2f, 2.3f, 22.4f,
+		3.2f, 3.2f, 4.4f, 5.34f,
+		3.49f, 1.1f, 1.4f, 12.1f,
+		77.f, 3.2f, 3.4f, 3.32f
+	);
+
+	mathz::Mat4 expected_subtraction(
+		-0.2f, 0.8f, 0.7f, -19.4f,
+		-1.2f, -0.2f, -3.4f, -2.34f,
+		-1.49f, 2.9f, -0.4f, -9.1f,
+		-75.f, 1.8f, -2.4f, -0.32f
+	);
+
+	mathz::Mat4 result = a - b;
+
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		for (unsigned int j = 0; j < 4; ++j)
+		{
+			REQUIRE(result(i, j) == Approx(expected_subtraction(i, j)).margin(0.0001).epsilon(1e-10));
+		}
+	}
+}
+
+TEST_CASE("Matrix-Matrix Multiplication", "[matrix 4x4]")
+{
+	mathz::Mat4 a(
+		273.f, 683.f, 792.f, 865.f,
+		-585.f, -281.f, 840.f, 19.f,
+		-125.f, -921.f, -840.f, 284.f,
+		927.f, 446.f, -966.f, -673.f
+	);
+
+	mathz::Mat4 b(
+		24.f, 307.f, -34.f, 801.f,
+		-894.f, 711.f, 940.f, 599.f,
+		-796.f, -976.f, -758.f, 781.f,
+		303.f, -451.f, 704.f, -70.f
+	);
+
+	mathz::Mat4 expected_multiplication(
+		-972387.f, -593683.f, 641362.f,	1185792.f,
+		-425709.f, -1207795.f, -867594.f,	17806.f,
+		1575066.f, -1450.f, -24834.f, -1327724.f,
+		188541.f,	1848034.f,	646158.f,	302345.f
+	);
+
+	mathz::Mat4 result = a * b;
+
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		for (unsigned int j = 0; j < 4; ++j)
+		{
+			REQUIRE(result(i, j) == Approx(expected_multiplication(i, j)).margin(0.0001).epsilon(1e-10));
+		}
+	}
+}
+
+TEST_CASE("Matrix-Vector Multiplication", "[matrix 4x4]")
+{
+	mathz::Mat4 a(
+		380.f, 881.f, 321.f, -405.f,
+		-88.f, 379.f, 395.f, -722.f,
+		936.f, -314.f, -846.f, 262.f,
+		554.f, -970.f, 389.f, -190.f
+	);
+
+	mathz::Vec3 b(
+		 265.f,
+		-810.f,
+		 233.f
+	);
+
+	mathz::Vec3 expected_multiplication(
+		 0.725589810365f,
+		-0.274276447894f,
+		-0.801733441565f
+	);
+
+	mathz::Vec3 result = a * b;
+
+	REQUIRE(result.x == expected_multiplication.x);
+	REQUIRE(result.y == expected_multiplication.y);
+	REQUIRE(result.z == expected_multiplication.z);
+}
+
+TEST_CASE("Matrix-Scalar Multiplication", "[matrix 4x4]")
+{
+	mathz::Mat4 a(
+		-369.f, 434.f, 463.f, -743.f,
+		-5.f, -397.f, 403.f, -822.f,
+		791.f, -994.f, -760.f, -177.f,
+		-233.f, -786.f, 868.f, -4
+	);
+
+	float scalar = 2.f;
+
+	mathz::Mat4 expected_multiplication(
+		-738.f, 868.f, 926.f, -1486.f,
+		-10.f, -794.f, 806.f, -1644.f,
+		1582.f, -1988.f, -1520.f, -354.f,
+		-466.f, -1572.f, 1736, -8
+	);
+
+	mathz::Mat4 result = a * scalar;
+
+	for (unsigned int i = 0; i < 4; ++i)
+	{
+		for (unsigned int j = 0; j < 4; ++j)
+		{
+			REQUIRE(result(i, j) == Approx(expected_multiplication(i, j)).margin(0.0001).epsilon(1e-10));
+		}
+	}
+}
